60b17e482c39c9fd2b90e4e0928efadedb7df262
Huy Tùng Nguyễn <tungnguyen151201@gmail.com>
2023-10-30 21:34:01 +0700

    listen port 80

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(8080, '0.0.0.0');
+  await app.listen(80);
 }
 bootstrap();
