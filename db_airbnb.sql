8d93c0c8467baf6d20270c1b265437da54d6932e
Huy Tùng Nguyễn <tungnguyen151201@gmail.com>
2023-10-30 20:02:17 +0700

    add docker files

--- db_airbnb.sql
+++ db_airbnb.sql
@@ -27,14 +27,14 @@ CREATE TABLE `Location` (
   `id` int NOT NULL AUTO_INCREMENT,
   `locationName` varchar(255) DEFAULT NULL,
   `province` varchar(255) DEFAULT NULL,
-  `country` int DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
   `image` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `Review`;
 CREATE TABLE `Review` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `roomId` int DEFAULT NULL,
   `userId` int DEFAULT NULL,
   `reviewDate` datetime DEFAULT NULL,
@@ -45,7 +45,7 @@ CREATE TABLE `Review` (
   KEY `userId` (`userId`),
   CONSTRAINT `Review_ibfk_1` FOREIGN KEY (`roomId`) REFERENCES `Room` (`id`),
   CONSTRAINT `Review_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `User` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `Room`;
 CREATE TABLE `Room` (
@@ -57,7 +57,7 @@ CREATE TABLE `Room` (
   `description` varchar(255) DEFAULT NULL,
   `price` int DEFAULT NULL,
   `washingMachine` tinyint(1) DEFAULT NULL,
-  `ironingBoard` tinyint(1) DEFAULT NULL,
+  `iron` tinyint(1) DEFAULT NULL,
   `television` tinyint(1) DEFAULT NULL,
   `airConditioner` tinyint(1) DEFAULT NULL,
   `wifi` tinyint(1) DEFAULT NULL,
@@ -71,7 +71,7 @@ CREATE TABLE `Room` (
   KEY `guest` (`guest`),
   KEY `location` (`locationId`),
   CONSTRAINT `Room_ibfk_1` FOREIGN KEY (`locationId`) REFERENCES `Location` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `User`;
 CREATE TABLE `User` (
@@ -83,18 +83,39 @@ CREATE TABLE `User` (
   `birthday` varchar(255) DEFAULT NULL,
   `gender` varchar(255) DEFAULT NULL,
   `role` varchar(255) DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 
+INSERT INTO `Location` (`id`, `locationName`, `province`, `country`, `image`) VALUES
+(1, 'Vung Tau', 'Ba Ria, Vung Tau', 'Viet Nam', '/public/img1698393981984_avt_meo.jpg');
+INSERT INTO `Location` (`id`, `locationName`, `province`, `country`, `image`) VALUES
+(2, 'Long Hai', 'Ba Ria, Vung Tau', 'Viet Nam', '/public/img1698394151557_avt_meo.jpg');
 
 
+INSERT INTO `Review` (`id`, `roomId`, `userId`, `reviewDate`, `star`, `detail`) VALUES
+(1, 1, 1, '2023-10-27 00:00:00', 4, 'test review');
+INSERT INTO `Review` (`id`, `roomId`, `userId`, `reviewDate`, `star`, `detail`) VALUES
+(3, 1, 1, '2023-10-27 00:00:00', 5, 'test review');
+INSERT INTO `Review` (`id`, `roomId`, `userId`, `reviewDate`, `star`, `detail`) VALUES
+(4, 3, 1, '2023-10-27 00:00:00', 5, 'test review');
+INSERT INTO `Review` (`id`, `roomId`, `userId`, `reviewDate`, `star`, `detail`) VALUES
+(5, 1, 3, '2023-10-27 00:00:00', 5, 'test review');
+
+INSERT INTO `Room` (`id`, `roomName`, `guest`, `bedroom`, `bed`, `description`, `price`, `washingMachine`, `iron`, `television`, `airConditioner`, `wifi`, `stove`, `parkingLot`, `swimmingPool`, `image`, `locationId`) VALUES
+(1, 'Sang xin min', 4, 1, 2, 'Phong sang xin min', 20, 1, 1, 1, 1, 1, 0, 1, 0, '/public/img1698398592270_avt_meo.jpg', 2);
+INSERT INTO `Room` (`id`, `roomName`, `guest`, `bedroom`, `bed`, `description`, `price`, `washingMachine`, `iron`, `television`, `airConditioner`, `wifi`, `stove`, `parkingLot`, `swimmingPool`, `image`, `locationId`) VALUES
+(3, 'Sang xin min', 4, 1, 2, 'Phong sang xin min', 20, 1, 1, 1, 1, 1, 0, 1, 0, NULL, 1);
 
 
-
-
-
+INSERT INTO `User` (`id`, `name`, `email`, `password`, `phone`, `birthday`, `gender`, `role`, `avatar`) VALUES
+(1, 'Admin', 'admin@gmail.com', '$2b$10$bU15KhVdRkFx7FeTm3SVHeIO4rNKuLlmf.iTjftlG62rPO6znLw.u', '0123456789', '01/10/2001', 'male', 'Admin', '/public/img1698382471450_avt_meo.jpg');
+INSERT INTO `User` (`id`, `name`, `email`, `password`, `phone`, `birthday`, `gender`, `role`, `avatar`) VALUES
+(3, 'User', 'user2@gmail.com', '$2b$10$SIAaqueVANc/VLK.0BXI9efkgQbimxo9MwmUN3h.WHB2JJleAwgYq', '0123456789', '01/10/2001', 'male', 'User', NULL);
+INSERT INTO `User` (`id`, `name`, `email`, `password`, `phone`, `birthday`, `gender`, `role`, `avatar`) VALUES
+(5, 'Admin', 'admin2@gmail.com', '$2b$10$9rd9tl/cPB0V2m.AG3jiXuzi/LfW1gEA9bjKxSoDtRJATEW2CUnte', '0123456789', '01/10/2001', 'Male', 'Admin', NULL);
 
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
